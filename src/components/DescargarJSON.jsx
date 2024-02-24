003fcc80a20001da56d41cb7f2cb433ac18f5fcb
AEstrada <allan.estrada9@gmail.com>
2024-02-23 21:24:15 -0600

    Corrigiendo sincronia a la hora de obtener la informacion y descargar el json

--- src/components/DescargarJSON.jsx
+++ src/components/DescargarJSON.jsx
@@ -1,72 +1,55 @@
 import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
 
 export default function DescargarJSON() {
-  const [fotoperfil, setFotoperfil] = useState("");
-  const [informacionpersonal, setInformacionpersonal] = useState([]);
-  const [informacionacademica, setInformacionacademica] = useState([]);
-  const [experiencialaboral, setExperiencialaboral] = useState([]);
-  const [competencias, setCompetencias] = useState([]);
-  const [referenciasprofesionales, setReferenciasprofesionales] = useState([]);
-  const [info, setInfo] = useState({
-    nombre: "Nombre",
-    apellido: "Apellido",
-  });
+  async function descargarArchivoJSON() {
+    try {
+      // Realizar verificaciones y asignaciones de datos desde las cookies
+      const fotoPerfil = localStorage.getItem("fotoPerfil");
+      const InformacionPersonal = Cookies.get("InformacionPersonal");
+      const InformacionAcademica = Cookies.get("InformacionAcademica");
+      const ExperienciaLaboral = Cookies.get("ExperienciaLaboral");
+      const Competencias = Cookies.get("Competencias");
+      const ReferenciasProfesionales = Cookies.get("ReferenciasProfesionales");
 
-  useEffect(() => {
-    const fotoPerfil = localStorage.getItem("fotoPerfil");
-    if (fotoPerfil) setFotoperfil(fotoPerfil);
+      const ip = JSON.parse(InformacionPersonal);
+      const info = {
+        nombre: ip.nombre || "Nombre",
+        apellido: ip.apellido || "Apellido",
+      };
 
-    const InformacionPersonal = Cookies.get("InformacionPersonal");
-    if (InformacionPersonal) {
-      setInformacionpersonal(InformacionPersonal);
-      setInfo(JSON.parse(InformacionPersonal));
+      // Asignar los datos a los estados correspondientes
+      const contenidoJSON = {
+        fotoPerfil: fotoPerfil || "",
+        Competencias: Competencias || [],
+        ExperienciaLaboral: ExperienciaLaboral || [],
+        InformacionPersonal: InformacionPersonal || {},
+        InformacionAcademica: InformacionAcademica || [],
+        ReferenciasProfesionales: ReferenciasProfesionales || [],
+      };
+
+      // Convertir el objeto JSON a una cadena JSON
+      const contenidoCadenaJSON = JSON.stringify(contenidoJSON, null, 2);
+      const blob = new Blob([contenidoCadenaJSON], {
+        type: "application/json",
+      });
+      const url = URL.createObjectURL(blob);
+
+      // Crear un enlace de descarga
+      const enlaceDescarga = document.createElement("a");
+      enlaceDescarga.href = url;
+      enlaceDescarga.download = `datos_${info.nombre}${info.apellido}.json`;
+
+      // Simular un clic en el enlace de descarga
+      document.body.appendChild(enlaceDescarga);
+      enlaceDescarga.click();
+
+      // Limpiar el objeto URL
+      URL.revokeObjectURL(url);
+      document.body.removeChild(enlaceDescarga);
+    } catch (error) {
+      console.error("Error al descargar el archivo JSON:", error);
     }
-
-    const InformacionAcademica = Cookies.get("InformacionAcademica");
-    if (InformacionAcademica) setInformacionacademica(InformacionAcademica);
-
-    const ExperienciaLaboral = Cookies.get("ExperienciaLaboral");
-    if (ExperienciaLaboral) setExperiencialaboral(ExperienciaLaboral);
-
-    const Competencias = Cookies.get("Competencias");
-    if (Competencias) setCompetencias(Competencias);
-
-    const ReferenciasProfesionales = Cookies.get("ReferenciasProfesionales");
-    if (ReferenciasProfesionales)
-      setReferenciasprofesionales(ReferenciasProfesionales);
-  }, []);
-
-  const contenidoJSON = {
-    fotoPerfil: fotoperfil,
-    Competencias: competencias,
-    ExperienciaLaboral: experiencialaboral,
-    InformacionPersonal: informacionpersonal,
-    InformacionAcademica: informacionacademica,
-    ReferenciasProfesionales: referenciasprofesionales,
-  };
-
-  // Convertir el objeto JSON a una cadena JSON
-  const contenidoCadenaJSON = JSON.stringify(contenidoJSON, null, 2);
-
-  // Manejar la descarga del archivo JSON
-  const descargarArchivoJSON = () => {
-    const blob = new Blob([contenidoCadenaJSON], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-
-    // Crear un enlace de descarga
-    const enlaceDescarga = document.createElement("a");
-    enlaceDescarga.href = url;
-    enlaceDescarga.download = `datos_${info.nombre}${info.apellido}.json`;
-
-    // Simular un clic en el enlace de descarga
-    document.body.appendChild(enlaceDescarga);
-    enlaceDescarga.click();
-
-    // Limpiar el objeto URL
-    URL.revokeObjectURL(url);
-    document.body.removeChild(enlaceDescarga);
-  };
+  }
 
   return (
     <div>
