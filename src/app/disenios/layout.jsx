b5417805912375013937498e0de869d8ce92eee0
AEstrada <allan.estrada9@gmail.com>
2024-05-25 16:11:53 -0600

    Corrigiendo centrado de elementos

--- src/app/disenios/layout.jsx
+++ src/app/disenios/layout.jsx
@@ -2,7 +2,7 @@ import SidebarDisenios from "@/components/disenios/SidebarDisenios";
 
 export default function layout({ children }) {
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start w-full">
+    <div className="flex flex-col md:flex-row items-center w-screen">
       <SidebarDisenios />
       <section className="md:px-10 md:flex-grow md:h-[85vh] overflow-y-auto antialiased">
         {children}
