0b77bd3bdb6adb782decbba5024667b5b7f0c982
AEstrada <allan.estrada9@gmail.com>
2024-05-30 20:47:34 -0600

    Aumentando tamaño de fuente

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function inicio() {
     <main className="p-5 md:p-0">
       <div className="flex h-max lg:h-[80vh] lg:items-center">
         <div className="flex flex-col gap-5 lg:gap-10">
-          <section className="flex flex-col justify-center items-center">
+          <section className="flex flex-col justify-center items-center md:text-base">
             {/* <h1
               className={`${titulo.className} antialiased text-3xl md:text-5xl font-bold lg:hidden`}
             >
